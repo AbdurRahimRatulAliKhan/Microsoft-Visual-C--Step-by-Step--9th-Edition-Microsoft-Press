f91974143f66643a838247ea4d2466e8b86bd26e
Ratul Ali <rahim498617@gmail.com>
2022-09-20 13:38:39 +0600

    Chapter 1

---
+++ VCSBS/Chapter 1/TestHello/TestHello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+//using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //System.Console.WriteLine("Hello World!");
+        }
+    }
+}
