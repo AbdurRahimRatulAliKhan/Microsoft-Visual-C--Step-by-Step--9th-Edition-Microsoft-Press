fc1b007940f1b7a178d9e84ad3214a87146bc8ce
Ratul Ali <rahim498617@gmail.com>
2022-09-28 09:21:14 +0600

    chapter 3

---
+++ Microsoft Press/VCSBS/Chapter 3/DailyRate Using Optional Parameters/DailyRate/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyRate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+
+        }
+    }
+}
---
+++ Microsoft Press/VCSBS/Chapter 3/Factorial - Complete/Factorial/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Program()).run();
+        }
+
+        void run()
+        {
+            Console.Write("Please enter a positive integer: ");
+            string inputValue =  Console.ReadLine();
+            long factorialValue = CalculateFactorial(inputValue);
+            Console.WriteLine($"Factorial({inputValue}) is {factorialValue}");
+        }
+
+        long CalculateFactorial(string input)
+        {
+            int inputValue = int.Parse(input); 
+            long factorial (int dataValue)
+            {
+                if (dataValue == 1)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return dataValue * factorial(dataValue - 1);
+                }
+            }
+
+            long factorialValue = factorial(inputValue);
+            return factorialValue;
+        } 
+    }
+}
