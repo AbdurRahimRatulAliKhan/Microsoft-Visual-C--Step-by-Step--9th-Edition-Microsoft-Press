ec607553c2de1cda54551bfd1aa926a098cc13e3
Ratul Ali <rahim498617@gmail.com>
2022-10-10 19:54:49 +0600

    chapter 7

---
+++ Microsoft Press/VCSBS/Chapter 7/Classes/Classes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void doWork()
+        {
+            // TODO:
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
