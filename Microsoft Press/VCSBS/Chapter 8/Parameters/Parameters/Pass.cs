32586e91c10bedf5fe21d1178cfce07ad98a01e6
Ratul Ali <rahim498617@gmail.com>
2022-10-11 20:12:31 +0600

    chapter 8

---
+++ Microsoft Press/VCSBS/Chapter 8/Parameters/Parameters/Pass.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Parameters
+{
+    class Pass
+    {
+        // TODO:
+    }
+}
---
+++ Microsoft Press/VCSBS/Chapter 8/Parameters/Parameters/WrappedInt.cs
@@ -0,0 +1,7 @@
+﻿namespace Parameters
+{
+    class WrappedInt
+    {
+        // TODO:
+    }
+}
