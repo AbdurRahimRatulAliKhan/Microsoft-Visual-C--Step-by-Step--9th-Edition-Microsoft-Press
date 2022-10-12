5dba9d084b0b2da49326322a67ccf43b7907a10e
Ratul Ali <rahim498617@gmail.com>
2022-10-12 18:29:38 +0600

    chapter 9

---
+++ Microsoft Press/VCSBS/Chapter 9/StructsAndEnums/StructsAndEnums/Program.cs
@@ -0,0 +1,32 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace StructsAndEnums
+{
+    class Program
+    {
+        static void doWork()
+        {
+            // TODO:
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
