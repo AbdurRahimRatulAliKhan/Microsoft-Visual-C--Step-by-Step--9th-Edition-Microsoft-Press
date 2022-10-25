d620ad9c967dc1c861f03f04e900bec719795fed
Ratul Ali <rahim498617@gmail.com>
2022-10-25 23:24:06 +0600

    chapter 10

---
+++ Microsoft Press/VCSBS/Chapter 11/ParamsArray/ParamsArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParamsArray
+{
+    class Program
+    {
+        static void doWork()
+        {
+            // TODO:		
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: {0}", ex.Message);
+            }
+        }
+    }
+}
